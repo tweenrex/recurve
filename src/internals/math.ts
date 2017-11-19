73052e81a88122abceeabd4473e5c35437106f45
Chris Johnson <jhnsn.c@gmail.com>
2017-11-18 21:26:28 -0600

    added normalizer functions

--- src/internals/math.ts
+++ src/internals/math.ts
@@ -1,5 +1,11 @@
-const math = Math
-export const cos = math.cos
-export const sin = math.sin
-export const sqrt = math.sqrt
-export const pow = math.pow
+const math = Math;
+export const min = math.min;
+export const max = math.max;
+export const cos = math.cos;
+export const sin = math.sin;
+export const sqrt = math.sqrt;
+export const pow = math.pow;
+
+export const clamp = function(n:number) {
+    return max(0.0, min(1.0, n));
+}
